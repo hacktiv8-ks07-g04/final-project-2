d6f1175049e60c6ccfc70168c11a9d04a1b44474
fhrlzmn <fhrlzmn26@gmail.com>
2023-11-13 22:42:41 +0700

    feat: create validation on Photo entity

--- domain/entity/photo.go
+++ domain/entity/photo.go
@@ -1,10 +1,23 @@
 package entity
 
+import (
+	"github.com/asaskevich/govalidator"
+)
+
 type Photo struct {
 	Base
-	Title    string `gorm:"not null; type:varchar(255)"`
+	Title    string `gorm:"not null; type:varchar(255)"                   valid:"required"`
 	Caption  string `gorm:"not null; type:varchar(255)"`
-	PhotoURL string `gorm:"not null; type:varchar(255)"`
+	PhotoURL string `gorm:"not null; type:varchar(255)"                   valid:"required"`
 	UserID   uint   `gorm:"not null; type:int"`
 	User     User   `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE"`
+}
+
+func (p *Photo) BeforeCreate() error {
+	_, err := govalidator.ValidateStruct(p)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
