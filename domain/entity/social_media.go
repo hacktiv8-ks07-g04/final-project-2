7b7edfc74d87a6719681f9e196db35b6f1f6a5ab
fhrlzmn <fhrlzmn26@gmail.com>
2023-11-13 22:44:31 +0700

    feat: create validation on SocialMedia entity

--- domain/entity/social_media.go
+++ domain/entity/social_media.go
@@ -1,9 +1,22 @@
 package entity
 
+import (
+	"github.com/asaskevich/govalidator"
+)
+
 type SocialMedia struct {
 	Base
-	Name           string `gorm:"not null; type:varchar(255)"`
-	SocialMediaURL string `gorm:"not null; type:varchar(255)"`
+	Name           string `gorm:"not null; type:varchar(255)"                   valid:"required"`
+	SocialMediaURL string `gorm:"not null; type:varchar(255)"                   valid:"required"`
 	UserID         uint   `gorm:"not null; type:int"`
 	User           User   `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE"`
+}
+
+func (sm *SocialMedia) BeforeCreate() error {
+	_, err := govalidator.ValidateStruct(sm)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
