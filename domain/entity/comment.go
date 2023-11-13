803f1671b7d1cc2e58feb0fb7a42ab376dba1357
fhrlzmn <fhrlzmn26@gmail.com>
2023-11-13 22:45:40 +0700

    feat: create validation on Comment entity

--- domain/entity/comment.go
+++ domain/entity/comment.go
@@ -1,10 +1,23 @@
 package entity
 
+import (
+	"github.com/asaskevich/govalidator"
+)
+
 type Comment struct {
 	Base
 	UserID  uint   `gorm:"not null; type:int"`
 	PhotoID uint   `gorm:"not null; type:int"`
-	Message string `gorm:"not null; type:varchar(255)"`
+	Message string `gorm:"not null; type:varchar(255)"                    valid:"required"`
 	User    User   `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE"`
 	Photo   Photo  `gorm:"foreignKey:PhotoID;constraint:OnUpdate:CASCADE"`
+}
+
+func (c *Comment) BeforeCreate() error {
+	_, err := govalidator.ValidateStruct(c)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
