1237fd39ee9e46b54663b904d63fed080daeaa0b
fhrlzmn <fhrlzmn26@gmail.com>
2023-11-13 22:38:28 +0700

    feat: create validation on User entity

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -1,9 +1,22 @@
 package entity
 
+import (
+	"github.com/asaskevich/govalidator"
+)
+
 type User struct {
 	Base
-	Username string `gorm:"not null; type:varchar(255)"`
-	Email    string `gorm:"not null; type:varchar(255)"`
-	Password string `gorm:"not null; type:varchar(255)"`
-	Age      uint   `gorm:"not null; type:int"`
+	Username string `gorm:"not null; type:varchar(255);unique" valid:"required"`
+	Email    string `gorm:"not null; type:varchar(255);unique" valid:"required, email"`
+	Password string `gorm:"not null; type:varchar(255)"        valid:"required, minstringlength(6)"`
+	Age      uint   `gorm:"not null; type:int"                 valid:"required, range(8|100)"`
+}
+
+func (u *User) BeforeCreate() error {
+	_, err := govalidator.ValidateStruct(u)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
