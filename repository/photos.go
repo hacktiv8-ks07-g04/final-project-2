80187ce72d2065602142e81e3b7a6188ddee36f1
fhrlzmn <fhrlzmn26@gmail.com>
2023-11-20 09:34:49 +0700

    feat: add get photo by ID func repository

--- repository/photos.go
+++ repository/photos.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"gorm.io/gorm"
 
 	"github.com/hacktiv8-ks07-g04/final-project-2/domain/entity"
@@ -8,6 +10,7 @@ import (
 
 type Photos interface {
 	Add(photo *entity.Photo) (*entity.Photo, error)
+	Get(photoId uint) (*entity.Photo, error)
 }
 
 type PhotosImpl struct {
@@ -25,4 +28,14 @@ func (r *PhotosImpl) Add(photo *entity.Photo) (*entity.Photo, error) {
 	}
 
 	return photo, err
+}
+
+func (r *PhotosImpl) Get(photoId uint) (*entity.Photo, error) {
+	var photo entity.Photo
+	err := r.db.First(&photo, photoId).Error
+	if err != nil {
+		return nil, errors.New("photo not found")
+	}
+
+	return &photo, err
 }
