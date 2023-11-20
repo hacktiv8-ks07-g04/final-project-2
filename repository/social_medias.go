6be831cbde9f9892368b152fe5313566bed7f03e
fhrlzmn <fhrlzmn26@gmail.com>
2023-11-20 17:53:48 +0700

    feat: create get socialmedia by id in repository

--- repository/social_medias.go
+++ repository/social_medias.go
@@ -1,15 +1,33 @@
 package repository
 
 import (
+	"errors"
+
 	"gorm.io/gorm"
+
+	"github.com/hacktiv8-ks07-g04/final-project-2/domain/entity"
 )
 
-type SocialMedias interface{}
+type SocialMedias interface {
+	Get(id uint) (*entity.SocialMedia, error)
+}
 
 type SocialMediasImpl struct {
 	db *gorm.DB
 }
 
 func NewSocialMedias(db *gorm.DB) *SocialMediasImpl {
-	return &SocialMediasImpl{db}
+	return &SocialMediasImpl{
+		db: db,
+	}
+}
+
+func (r *SocialMediasImpl) Get(id uint) (*entity.SocialMedia, error) {
+	socialMedia := entity.SocialMedia{}
+
+	if err := r.db.First(&socialMedia, id).Error; err != nil {
+		return nil, errors.New("social media not found")
+	}
+
+	return &socialMedia, nil
 }
