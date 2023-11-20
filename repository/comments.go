e40b31261f4ef5a6d21b2e2798c066cbf4778892
fhrlzmn <fhrlzmn26@gmail.com>
2023-11-20 13:35:32 +0700

    feat: create get comment by id in repository

--- repository/comments.go
+++ repository/comments.go
@@ -2,9 +2,13 @@ package repository
 
 import (
 	"gorm.io/gorm"
+
+	"github.com/hacktiv8-ks07-g04/final-project-2/domain/entity"
 )
 
-type Comments interface{}
+type Comments interface {
+	Get(id uint) (*entity.Comment, error)
+}
 
 type CommentsImpl struct {
 	db *gorm.DB
@@ -12,4 +16,14 @@ type CommentsImpl struct {
 
 func NewComments(db *gorm.DB) *CommentsImpl {
 	return &CommentsImpl{db}
+}
+
+func (r *CommentsImpl) Get(id uint) (*entity.Comment, error) {
+	comment := entity.Comment{}
+
+	if err := r.db.First(&comment, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &comment, nil
 }
